f26328f5888bbb2edfcd595ef1bfc08f938c589c
datlt2306 <datlt2306@gmail.com>
2025-09-17 17:41:46 +0700

    feat(todos): thêm danh sách công việc vào ứng dụng
    - Thêm interface ITodo để định nghĩa kiểu dữ liệu cho công việc.
    - Khởi tạo danh sách công việc với các mục công việc mẫu trong state của ứng dụng.
    - Cập nhật logic để quản lý trạng thái của danh sách công việc.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 
+interface ITodo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 function App() {
+    const todosList: ITodo[] = [
+        { id: 1, title: "Todo 1", completed: false },
+        { id: 2, title: "Todo 2", completed: false },
+        { id: 3, title: "Todo 3", completed: false },
+    ];
     const [count, setCount] = useState<number>(0);
     const [color, setColor] = useState<string>("red");
+    const [todos, setTodos] = useState<ITodo[]>(todosList);
     return (
         <>
             <div style={{ backgroundColor: color, width: 200, height: 200 }}></div>
